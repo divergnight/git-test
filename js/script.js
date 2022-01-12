c523e43e58efa89ff1f6d3b14bc4cbba2f9450a5
divergnight <47478344+divergnight@users.noreply.github.com>
2022-01-12 10:21:34 +0100

    Dernier Correctif

--- js/script.js
+++ js/script.js
@@ -6,3 +6,5 @@ var dataSpyList = [].slice.call(document.querySelectorAll('[data-bs-spy="scroll"
 dataSpyList.forEach(function (dataSpyEl) {
 bootstrap.ScrollSpy.getInstance(dataSpyEl).refresh()
 })
+
+sudo certbot certonly --non-interactive --email matisse.bouchy@gmail.com --preferred-challenges http --standalone --agree-tos --renew-by-default --webroot-path /var/www/vadn.ovh/ -d *.example.com --dry-run
